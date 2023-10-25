153c5a874e9a2072bc0bb2d085bab8e8180e0a07
ValentinImmobilier <raphaelpayet@twinpaw.fr>
2023-10-25 16:37:09 +0200

    Initial commit

---
+++ ClimApp/ClimAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimAppApp.swift
+//  ClimApp
+//
+//  Created by Raphaël Payet on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClimAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
